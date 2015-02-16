b005fa78026e68a37ef72466ccbb5c3cdf6f7094
Sean Deneen <speedygonzalezz22@yahoo.com>
2015-02-15 21:11:37 -0500

    is_possible_to_win restricted case passed simple tests as well

--- test_suite.py
+++ test_suite.py
@@ -199,3 +199,40 @@ walls = [{'wallX': 5, 'wallY': 4, 'wallO': 'V'},
 
 print is_possible_to_win(players[myId], myId, walls, "DOWN")
 print "^should be false by trying to go through blocked gap down^"
+
+
+# Simple test isn't possible down
+walls = [{'wallX': 5, 'wallY': 4, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 6, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 8, 'wallO': 'H'},
+         {'wallX': 7, 'wallY': 7, 'wallO': 'V'}]
+
+print is_possible_to_win(players[myId], myId, walls, "DOWN")
+print "^should be false by trying to go through blocked gap down^"
+
+# Simple test is possible down
+walls = [{'wallX': 5, 'wallY': 4, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 6, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 8, 'wallO': 'H'}]
+
+print is_possible_to_win(players[myId], myId, walls, "DOWN")
+print "^should be true by going through gap down^"
+
+
+# Simple test win is possible with gap up
+walls = [{'wallX': 5, 'wallY': 5, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 3, 'wallO': 'V'}]
+
+print is_possible_to_win(players[myId], myId, walls, "UP")
+print "^should be true by going through gap up^"
+
+
+# Simple test win isn't possible with gap up
+walls = [{'wallX': 5, 'wallY': 5, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 3, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 1, 'wallO': 'V'},
+         {'wallX': 5, 'wallY': 1, 'wallO': 'H'},
+         {'wallX': 7, 'wallY': 0, 'wallO': 'V'}]
+
+print is_possible_to_win(players[myId], myId, walls, "UP")
+print "^should be false by trying to go through blocked gap up^"
